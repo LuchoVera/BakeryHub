2a6f205a131da994d7c1240766aaeafc954c8e73
LuchoVera <luchito.espinoza11@gmail.com>
2025-04-10 18:21:44 -0400

    feat: add program and settings

---
+++ BakeryHub.Api/Program.cs
@@ -0,0 +1,147 @@
+using BakeryHub.Api.Middleware;
+using BakeryHub.Infrastructure.Persistence;
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using BakeryHub.Application.Interfaces;
+using BakeryHub.Application.Services;
+using BakeryHub.Domain.Interfaces;
+using BakeryHub.Infrastructure.Persistence.Repositories;
+using BakeryHub.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.SetIsOriginAllowed(origin =>
+                                 origin != null && (
+                                    origin.EndsWith(".localhost:5173") ||
+                                    origin.Equals("http://localhost:5173")
+                                 )
+                              )
+                             .AllowAnyHeader()
+                             .AllowAnyMethod()
+                             .AllowCredentials();
+                      });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "BakeryHub API", Version = "v1" });
+
+    options.AddSecurityDefinition("TenantIdHeader", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = TenantResolutionMiddleware.TenantIdHeaderName,
+        Type = SecuritySchemeType.ApiKey,
+        Description = "Tenant ID: (ej: 'tenantA', 'tenantB')"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "TenantIdHeader"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+builder.Services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
+{
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+    var connectionString = configuration.GetConnectionString("DefaultConnection"); 
+    options.UseNpgsql(connectionString);
+});
+
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 4;
+
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    options.User.RequireUniqueEmail = true;
+
+    options.SignIn.RequireConfirmedEmail = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Events = new CookieAuthenticationEvents
+    {
+        OnRedirectToLogin = context =>
+        {
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            return Task.CompletedTask;
+        },
+        OnRedirectToAccessDenied = context =>
+        {
+            context.Response.StatusCode = StatusCodes.Status403Forbidden;
+            return Task.CompletedTask;
+        }
+    };
+
+    options.Cookie.HttpOnly = true; 
+    options.Cookie.SecurePolicy = CookieSecurePolicy.None;
+    options.Cookie.SameSite = SameSiteMode.Lax;
+    options.SlidingExpiration = true; 
+    options.ExpireTimeSpan = TimeSpan.FromDays(14);
+    
+});
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ITenantRepository, TenantRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>(); 
+
+builder.Services.AddScoped<ITenantService, TenantService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>(); 
+builder.Services.AddScoped<ITenantManagementService, TenantManagementService>(); 
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "MultiTenant API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+//app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseMiddleware<TenantResolutionMiddleware>();
+
+app.MapControllers();
+app.MapGet("/api/ping", () => "Pong desde API!");
+
+app.Run();
