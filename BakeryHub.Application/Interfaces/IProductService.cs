d193798c168829541ff93eaf2165279984854d32
LuchoVera <luchito.espinoza11@gmail.com>
2025-04-25 17:07:26 -0400

    feat: add new methos in services for delete and public get

--- BakeryHub.Application/Interfaces/IProductService.cs
+++ BakeryHub.Application/Interfaces/IProductService.cs
@@ -4,11 +4,13 @@ namespace BakeryHub.Application.Interfaces;
 
 public interface IProductService
 {
-    Task<IEnumerable<ProductDto>> GetAvailableProductsForAdminAsync(Guid adminTenantId);
+    Task<IEnumerable<ProductDto>> GetAllProductsForAdminAsync(Guid adminTenantId);
     Task<IEnumerable<ProductDto>> GetAvailableProductsByCategoryForAdminAsync(Guid categoryId, Guid adminTenantId);
     Task<ProductDto?> GetProductByIdForAdminAsync(Guid productId, Guid adminTenantId);
     Task<ProductDto?> CreateProductForAdminAsync(CreateProductDto productDto, Guid adminTenantId);
     Task<bool> UpdateProductForAdminAsync(Guid productId, UpdateProductDto productDto, Guid adminTenantId);
     Task<bool> SetProductAvailabilityForAdminAsync(Guid productId, bool isAvailable, Guid adminTenantId);
     Task<IEnumerable<ProductDto>> GetPublicProductsByTenantIdAsync(Guid tenantId);
+    Task<bool> DeleteProductForAdminAsync(Guid productId, Guid adminTenantId);
+    Task<ProductDto?> GetPublicProductByIdAsync(Guid productId, Guid tenantId);
 }
