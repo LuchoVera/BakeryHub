b48bd3ff88ada63f8b6d3cb86b637f1f04025b3b
LuchoVera <luchito.espinoza11@gmail.com>
2025-05-04 22:07:41 -0400

    feat: add filter to repositories

--- BakeryHub.Domain/Interfaces/IProductRepository.cs
+++ BakeryHub.Domain/Interfaces/IProductRepository.cs
@@ -5,11 +5,21 @@ namespace BakeryHub.Domain.Interfaces;
 public interface IProductRepository
 {
     Task<IEnumerable<Product>> GetAvailableProductsByTenantAsync(string tenantIdString);
-    Task<IEnumerable<Product>> GetAllProductsByTenantIdAsync(Guid tenantId);
-    Task<IEnumerable<Product>> GetAvailableProductsByCategoryAndTenantGuidAsync(Guid categoryId, Guid tenantId);
+    Task<IEnumerable<Product>> GetAllProductsByTenantIdAsync(
+            Guid tenantId,
+            string? searchTerm = null,
+            Guid? categoryId = null,
+            decimal? minPrice = null,
+            decimal? maxPrice = null
+        ); Task<IEnumerable<Product>> GetAvailableProductsByCategoryAndTenantGuidAsync(Guid categoryId, Guid tenantId);
     Task<Product?> GetByIdAsync(Guid productId);
     Task AddAsync(Product product);
     void Update(Product product);
     Task DeleteAsync(Guid productId);
-    Task<IEnumerable<Product>> SearchPublicProductsByNameAsync(Guid tenantId, string searchTerm);
+    Task<IEnumerable<Product>> SearchPublicProductsByNameAsync(
+            Guid tenantId,
+            string searchTerm,
+            Guid? categoryId = null,
+            decimal? minPrice = null,
+            decimal? maxPrice = null);
 }
