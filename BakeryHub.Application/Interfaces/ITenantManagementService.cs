c6e416a80c93b33f5231dc459c5f2f0de48ceb62
LuchoVera <luchito.espinoza11@gmail.com>
2025-06-24 01:03:32 -0400

    feat: update interface for tenant management

--- BakeryHub.Application/Interfaces/ITenantManagementService.cs
+++ BakeryHub.Application/Interfaces/ITenantManagementService.cs
@@ -1,7 +1,13 @@
 using BakeryHub.Application.Dtos;
+using BakeryHub.Application.Dtos.Theme;
 namespace BakeryHub.Application.Interfaces;
+
 public interface ITenantManagementService
 {
     Task<TenantDto?> GetTenantForAdminAsync(Guid adminUserId);
     Task<bool> UpdateTenantForAdminAsync(Guid adminUserId, UpdateTenantDto tenantDto);
+    Task<TenantThemeDto> GetThemeForAdminAsync(Guid adminUserId);
+    Task<bool> UpdateThemeForAdminAsync(Guid adminUserId, TenantThemeDto themeDto);
+    Task<bool> ResetPublicThemeAsync(Guid adminUserId);
+    Task<bool> ResetAdminThemeAsync(Guid adminUserId);
 }
