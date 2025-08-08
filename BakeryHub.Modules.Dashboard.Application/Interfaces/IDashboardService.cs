52c9f5364eeb32b389295850228b1e8315c363c3
LuchoVera <luchito.espinoza11@gmail.com>
2025-08-08 02:25:52 -0400

    refactor: move interfaces to dashboard module

---
+++ BakeryHub.Modules.Dashboard.Application/Interfaces/IDashboardService.cs
@@ -0,0 +1,9 @@
+﻿
+using BakeryHub.Modules.Dashboard.Application.Dtos.Dashboard;
+
+namespace BakeryHub.Modules.Dashboard.Application.Interfaces;
+
+public interface IDashboardService
+{
+    Task<DashboardResponseDto> GetDashboardStatisticsAsync(Guid tenantId, DashboardQueryParametersDto queryParams);
+}
