ef0b196e3af221ce957612f1f97406c06162c05a
LuchoVera <luchito.espinoza11@gmail.com>
2025-08-08 02:26:52 -0400

    refactor: add db config to dashboard module

---
+++ BakeryHub.Modules.Dashboard.Infrastructure/DashboardModuleExtensions.cs
@@ -0,0 +1,14 @@
+﻿using BakeryHub.Modules.Dashboard.Application.Interfaces;
+using BakeryHub.Modules.Dashboard.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BakeryHub.Modules.Dashboard.Infrastructure;
+
+public static class DashboardModuleExtensions
+{
+    public static IServiceCollection AddDashboardModule(this IServiceCollection services)
+    {
+        services.AddScoped<IDashboardService, DashboardService>();
+        return services;
+    }
+}
