c287c1c5dba1f53bafc9e6aaa7454615dff3523a
LuchoVera <luchito.espinoza11@gmail.com>
2025-08-08 02:23:57 -0400

    refactor: add models to catalog module

---
+++ BakeryHub.Modules.Catalog.Domain/Models/ProductTag.cs
@@ -0,0 +1,9 @@
+namespace BakeryHub.Modules.Catalog.Domain.Models;
+public class ProductTag
+{
+    public Guid ProductId { get; set; }
+    public virtual Product Product { get; set; } = null!;
+
+    public Guid TagId { get; set; }
+    public virtual Tag Tag { get; set; } = null!;
+}
---
+++ BakeryHub.Modules.Catalog.Domain/Models/Tag.cs
@@ -0,0 +1,18 @@
+using BakeryHub.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace BakeryHub.Modules.Catalog.Domain.Models;
+
+public class Tag
+{
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public required string Name { get; set; }
+    public Guid TenantId { get; set; }
+    public virtual Tenant Tenant { get; set; } = null!;
+    public virtual ICollection<ProductTag> ProductTags { get; set; } = new List<ProductTag>();
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset UpdatedAt { get; set; } = DateTimeOffset.UtcNow;
+}
