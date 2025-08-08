2bf0deac8d7276cca8004eed0604e4f1a75d92aa
LuchoVera <luchito.espinoza11@gmail.com>
2025-08-08 02:17:57 -0400

    refactor: add interfaces to account domain module

---
+++ BakeryHub.Modules.Accounts.Domain/Interfaces/ICustomerTenantMembershipRepository.cs
@@ -0,0 +1,9 @@
+﻿using BakeryHub.Modules.Accounts.Domain.Models;
+
+namespace BakeryHub.Modules.Accounts.Domain.Interfaces;
+
+public interface ICustomerTenantMembershipRepository
+{
+    Task<bool> IsMemberAsync(Guid userId, Guid tenantId);
+    Task AddAsync(CustomerTenantMembership membership);
+}
